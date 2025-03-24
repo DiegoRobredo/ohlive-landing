a763dc4c8ab9b16e12e7dad94a483cb716407cbb
Diego Robredo <drobredo@gmail.com>
2025-03-24 23:18:48 +0100

    Hero

--- src/app/components/SectionContainer.tsx
+++ src/app/components/SectionContainer.tsx
@@ -3,11 +3,16 @@ import { ReactNode } from "react";
 interface Props {
   readonly children: ReactNode;
   readonly sectionId: string;
+  readonly className?: string;
 }
 
-export default function SectionContainer({ children, sectionId }: Props) {
+export default function SectionContainer({
+  children,
+  sectionId,
+  className = ""
+}: Props) {
   return (
-    <section id={sectionId} className="max-w-6xl bg-transparent">
+    <section id={sectionId} className={`max-w-6xl p-8 ${className}`}>
       {children}
     </section>
   );
--- src/app/home/sections/Hero.tsx
+++ src/app/home/sections/Hero.tsx
@@ -2,8 +2,21 @@ import SectionContainer from "@/app/components/SectionContainer";
 
 export default function Hero() {
   return (
-    <SectionContainer sectionId="hero">
-      <div>Hero</div>
+    <SectionContainer sectionId="hero" className="mt-16">
+      <h1 className="text-4xl font-bold color-secondary">
+        Tus muebles pueden estar en casa del cliente antes de que los compre.
+        IKEA ya lo hace. ¿Y tú?
+      </h1>
+      <p>
+        Haz que tus clientes vean tus muebles en su salón, su cocina o su
+        dormitorio. Sin montajes. Sin Photoshop.
+      </p>
+      <p>
+        Con nuestra app de realidad aumentada, conviertes su móvil en tu mejor
+        vendedor. Vas a vender más, vas a recibir menos devoluciones… y vas a
+        dejar de parecer la tienda de siempre. Porque si no te modernizas tú, lo
+        hará la competencia.
+      </p>
     </SectionContainer>
   );
 }
