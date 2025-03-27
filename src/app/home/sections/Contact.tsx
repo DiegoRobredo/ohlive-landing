7011753128ec1bebc96b27c1f0bb455189252db3
Diego Robredo <drobredo@gmail.com>
2025-03-27 23:09:23 +0100

    Sección contacto

--- src/app/home/sections/Contact.tsx
+++ src/app/home/sections/Contact.tsx
@@ -3,7 +3,21 @@ import SectionContainer from "@/app/components/SectionContainer";
 export default function Contact() {
   return (
     <SectionContainer sectionId="contact">
-      <div>Contact</div>
+      <h2 className="text-3xl text-secondary font-bold">Contacto</h2>
+      <p className="text-secondary">
+        ¿Tienes alguna? Ponte en contacto con nosotros
+      </p>
+      <input
+        className="bg-secondary w-full p-2 mt-2"
+        placeholder="Tu email"
+      ></input>
+      <textarea
+        className="bg-secondary w-full p-2 mt-2"
+        placeholder="Tu mensaje"
+      ></textarea>
+      <button className="p-2 bg-secondary text-primary rounded-lg w-full text-xl">
+        Enviar
+      </button>
     </SectionContainer>
   );
 }
--- src/app/home/sections/FAQs.tsx
+++ src/app/home/sections/FAQs.tsx
@@ -3,7 +3,7 @@ import SectionContainer from "@/app/components/SectionContainer";
 export default function FAQs() {
   return (
     <SectionContainer sectionId="faqs">
-      <h2 className="text-3xl">FAQs</h2>
+      <h2 className="text-3xl font-bold text-secondary">FAQs</h2>
       <h3 className="text-xl text-secondary">¿Qué es Ohlive?</h3>
       <p className="text-secondary">
         Ohlive es una aplicación que te permite ver en vivo cómo quedarían los
@@ -14,19 +14,29 @@ export default function FAQs() {
         Ohlive utiliza la cámara de tu móvil para superponer los muebles en la
         estancia que desees.
       </p>
-      <h3 className="text-xl text-secondary">¿Qué necesito para usar Ohlive?</h3>
-      <p className="text-secondary">Solo necesitas un móvil con cámara y conexión a internet.</p>
-      <h3 className="text-xl text-secondary">¿Qué necesito para tener Ohlive en mi tienda Online?</h3>
+      <h3 className="text-xl text-secondary">
+        ¿Qué necesito para usar Ohlive?
+      </h3>
+      <p className="text-secondary">
+        Solo necesitas un móvil con cámara y conexión a internet.
+      </p>
+      <h3 className="text-xl text-secondary">
+        ¿Qué necesito para tener Ohlive en mi tienda Online?
+      </h3>
       <p className="text-secondary">
         Solo necesitas cargar tus modelos 3D en la plataforma de Ohlive y ya
         podrás disfrutar del visor de realidad aumentada en tu tienda.
       </p>
-      <h3 className="text-xl text-secondary">No tengo modelos en 3D. ¿Qué hago?</h3>
+      <h3 className="text-xl text-secondary">
+        No tengo modelos en 3D. ¿Qué hago?
+      </h3>
       <p className="text-secondary">
         No te preocupes, nosotros tenemos un equipo de diseñadores que pueden
         ayudarte a generar tus modelos 3D
       </p>
-      <h3 className="text-xl text-secondary">¿Cómo puedo tener Ohlive en mi tienda Online?</h3>
+      <h3 className="text-xl text-secondary">
+        ¿Cómo puedo tener Ohlive en mi tienda Online?
+      </h3>
       <p className="text-secondary">
         Contáctanos a través de nuestro formulario de contacto y te ayudaremos a
         integrar Ohlive en tu tienda.
