cee6ba258af43df747a47207d4921ac9487cb8d3
Diego Robredo <drobredo@gmail.com>
2025-04-03 10:49:50 +0200

    Hover effect on mobile card

--- src/app/home/sections/Hero/Card/index.tsx
+++ src/app/home/sections/Hero/Card/index.tsx
@@ -1,6 +1,27 @@
+"use client";
+
+import { useEffect } from "react";
+import { $ } from "@/app/lib/dom-selector";
 import "./styles.css";
 
 export default function MobileCard() {
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          entries[0].target.classList.add("card-motion-class");
+        } else {
+          entries[0].target.classList.remove("card-motion-class");
+        }
+      },
+      { rootMargin: "-50% 0px" }
+    );
+
+    const $card = $(".card-class") as HTMLImageElement;
+
+    observer.observe($card);
+  }, []);
+
   return (
     <article className="card-class ml-auto mr-auto mt-4 mb-4">
       <img
